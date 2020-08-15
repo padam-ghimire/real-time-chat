c856749fe8057008b2b9b63729288f6ac7bf6db3
Padam Ghimire <padamghimire75@gmail.com>
2020-08-15 15:42:31 +0545

    Project:All needed tables and factory created

---
+++ database/migrations/2020_08_15_094318_create_chats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('session_id');
+            $table->unsignedInteger('message_id');
+            $table->unsignedInteger('user_id');
+            $table->boolean('type'); //0 for received message and 1 for sent message
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        factory(User::class)->create(
+            [
+            'email'=>'padamghimire75@gmail.com',
+            'name'=>'Padam Ghimire'
+             ]
+        );
+
+        factory(User::class)->create(
+            [
+            'email'=>'padamghimire2020@gmail.com',
+            'name'=>'Niraj Ghimire'
+             ]
+        );
+
+        factory(User::class)->create(
+            [
+            'email'=>'ramana@gmail.com',
+            'name'=>'Raman Lamichanne'
+             ]
+        );
     }
 }
